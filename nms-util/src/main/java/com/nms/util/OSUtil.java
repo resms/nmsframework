a33fbe1b49134ea35ce758440710b2a821ec5c2e
dxzhan <zhandex@163.com>
2017-03-19 23:47:15 +0800

    加入操作系统工具类

--- nms-util/src/main/java/com/nms/util/OSUtil.java
+++ nms-util/src/main/java/com/nms/util/OSUtil.java
@@ -1,9 +1,11 @@
 package com.nms.util;
 
+import com.nms.common.OSEnv;
+
 /**
  * Created by sam on 17-3-19.
  */
-public class OSUtil
+public class OSUtil extends OSEnv
 {
     public static String getOSName() {
         return System.getProperty("os.name").toLowerCase();
