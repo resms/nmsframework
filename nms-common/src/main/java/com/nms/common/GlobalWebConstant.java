ef804781fe8dd5271051acb6daa4c9e379012d59
dxzhan <zhandex@163.com>
2017-03-19 23:40:49 +0800

    加入Web常量虚基类

---
+++ nms-common/src/main/java/com/nms/common/GlobalWebConstant.java
@@ -0,0 +1,29 @@
+package com.nms.common;
+
+/**
+ * Created by sam on 16-12-15.
+ */
+public abstract class GlobalWebConstant extends GlobalConstant
+{
+    public static final String MEDIA_TYPE_APPLICATION_XML = "application/xml";
+    public static final String MEDIA_TYPE_APPLICATION_XML_UTF_8 = "application/xml; charset=UTF-8";
+
+    public static final String MEDIA_TYPE_JSON = "application/json";
+    public static final String MEDIA_TYPE_JSON_UTF_8 = "application/json; charset=UTF-8";
+
+    public static final String MEDIA_TYPE_JAVASCRIPT = "application/javascript";
+    public static final String MEDIA_TYPE_JAVASCRIPT_UTF_8 = "application/javascript; charset=UTF-8";
+
+    public static final String MEDIA_TYPE_APPLICATION_XHTML_XML = "application/xhtml+xml";
+    public static final String MEDIA_TYPE_APPLICATION_XHTML_XML_UTF_8 = "application/xhtml+xml; charset=UTF-8";
+
+    public static final String MEDIA_TYPE_TEXT_PLAIN = "text/plain";
+    public static final String MEDIA_TYPE_TEXT_PLAIN_UTF_8 = "text/plain; charset=UTF-8";
+
+    public static final String MEDIA_TYPE_TEXT_XML = "text/xml";
+    public static final String MEDIA_TYPE_TEXT_XML_UTF_8 = "text/xml; charset=UTF-8";
+
+    public static final String MEDIA_TYPE_TEXT_HTML = "text/html";
+    public static final String MEDIA_TYPE_TEXT_HTML_UTF_8 = "text/html; charset=UTF-8";
+
+}
