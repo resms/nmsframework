1ec6ca65a93abf69388fddade1274cb266cfd553
dxzhan <zhandex@163.com>
2017-04-06 11:15:09 +0800

    修改内容常量名和加入HTTP action方法定义

--- nms-common/src/main/java/com/nms/common/GlobalWebConstant.java
+++ nms-common/src/main/java/com/nms/common/GlobalWebConstant.java
@@ -5,25 +5,36 @@ package com.nms.common;
  */
 public abstract class GlobalWebConstant extends GlobalConstant
 {
-    public static final String MEDIA_TYPE_APPLICATION_XML = "application/xml";
-    public static final String MEDIA_TYPE_APPLICATION_XML_UTF_8 = "application/xml; charset=UTF-8";
+    public static final String HTTP_METHOD_POST = "POST";
+    public static final String HTTP_METHOD_GET = "GET";
+    public static final String HTTP_METHOD_PUT = "PUT";
+    public static final String HTTP_METHOD_DELETE = "DELETE";
+    public static final String HTTP_METHOD_TRACE = "TRACE";
+    public static final String HTTP_METHOD_HEAD = "HEAD";
+    public static final String HTTP_METHOD_OPTIONS = "OPTIONS";
+    public static final String HTTP_METHOD_CONNECT = "CONNECT";
 
-    public static final String MEDIA_TYPE_JSON = "application/json";
-    public static final String MEDIA_TYPE_JSON_UTF_8 = "application/json; charset=UTF-8";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_XML_UTF_8 = "application/xml; charset=UTF-8";
 
-    public static final String MEDIA_TYPE_JAVASCRIPT = "application/javascript";
-    public static final String MEDIA_TYPE_JAVASCRIPT_UTF_8 = "application/javascript; charset=UTF-8";
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_JSON_UTF_8 = "application/json; charset=UTF-8";
 
-    public static final String MEDIA_TYPE_APPLICATION_XHTML_XML = "application/xhtml+xml";
-    public static final String MEDIA_TYPE_APPLICATION_XHTML_XML_UTF_8 = "application/xhtml+xml; charset=UTF-8";
+    public static final String CONTENT_TYPE_JAVASCRIPT = "application/javascript";
+    public static final String CONTENT_TYPE_JAVASCRIPT_UTF_8 = "application/javascript; charset=UTF-8";
 
-    public static final String MEDIA_TYPE_TEXT_PLAIN = "text/plain";
-    public static final String MEDIA_TYPE_TEXT_PLAIN_UTF_8 = "text/plain; charset=UTF-8";
+    public static final String CONTENT_TYPE_XHTML_XML = "application/xhtml+xml";
+    public static final String CONTENT_TYPE_XHTML_XML_UTF_8 = "application/xhtml+xml; charset=UTF-8";
 
-    public static final String MEDIA_TYPE_TEXT_XML = "text/xml";
-    public static final String MEDIA_TYPE_TEXT_XML_UTF_8 = "text/xml; charset=UTF-8";
+    public static final String CONTENT_TYPE_TEXT_PLAIN = "text/plain";
+    public static final String CONTENT_TYPE_TEXT_PLAIN_UTF_8 = "text/plain; charset=UTF-8";
 
-    public static final String MEDIA_TYPE_TEXT_HTML = "text/html";
-    public static final String MEDIA_TYPE_TEXT_HTML_UTF_8 = "text/html; charset=UTF-8";
+    public static final String CONTENT_TYPE_TEXT_XML = "text/xml";
+    public static final String CONTENT_TYPE_TEXT_XML_UTF_8 = "text/xml; charset=UTF-8";
 
+    public static final String CONTENT_TYPE_TEXT_HTML = "text/html";
+    public static final String CONTENT_TYPE_TEXT_HTML_UTF_8 = "text/html; charset=UTF-8";
+
+    public static final String CONTENT_TYPE_FORM = "application/x-www-form-urlencoded";
+    public static final String CONTENT_TYPE_FORM_UTF_8 = "application/x-www-form-urlencoded; charset=UTF-8";
 }
