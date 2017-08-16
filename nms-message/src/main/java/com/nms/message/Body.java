aecc8879e01c87b52ca5d2a5a3664a69c2a297bc
zhandx22711 <zhandx22711@hundsun.com>
2017-08-16 10:21:39 +0800

    加入注释

--- nms-message/src/main/java/com/nms/message/Body.java
+++ nms-message/src/main/java/com/nms/message/Body.java
@@ -6,6 +6,7 @@ package com.nms.message;
 import java.io.Serializable;
 
 /**
+ * 消息体接口
  * @author dxzhan
  */
 public interface Body extends Serializable
--- nms-message/src/main/java/com/nms/message/Header.java
+++ nms-message/src/main/java/com/nms/message/Header.java
@@ -6,13 +6,32 @@ package com.nms.message;
 import java.io.Serializable;
 
 /**
+ * 消息头接口
  * @author dxzhan
  */
 public interface Header extends Serializable
 {
+    /**
+     * 获得操作码
+     * @return
+     */
 	String getOpCode();
+
+    /**
+     * 设置操作码
+     * @param opCode
+     */
 	void setOpCode(String opCode);
-	
-	int getVersion();	
+
+    /**
+     * 获得版本号
+     * @return
+     */
+	int getVersion();
+
+    /**
+     * 设置版本号
+     * @param version
+     */
 	void setVersion(Integer version);
 }
